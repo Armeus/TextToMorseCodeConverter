a7480c18f8777aee42d801db3f592001756c747e
Jordan Smith <jordan0smith7@gmail.com>
2024-02-15 09:23:02 -0600

    update program run till user exits move conversion code to function

--- main.py
+++ main.py
@@ -5,12 +5,29 @@ import pandas as pd
 conversion_data = pd.read_csv('MorseCodeConversionChart.csv')
 conversion_dict = {row.Char: row.Morse for (index, row) in conversion_data.iterrows()}
 
-text = input('Enter Text: ').upper()
+
 # Example output for 'Hello World!': '.... . .-.. .-.. --- / .-- --- .-. .-.. -.. -.-.--'
-converted_text = ''
-for letter in text:
-    if letter in conversion_dict:
-        converted_text += f'{conversion_dict[letter]} '
-    elif letter == ' ':
-        converted_text += '/ '
-print(converted_text)
+def convert_text(text):
+    converted_text = ''
+    for letter in text:
+        if letter in conversion_dict:
+            converted_text += f'{conversion_dict[letter]} '
+        elif letter == ' ':
+            converted_text += '/ '
+    return converted_text
+
+
+continue_conversion = True
+
+# Program will run till user quits
+while continue_conversion:
+    text_input = input('Enter Text (Type exit to quit): ').upper()
+
+    if text_input == 'EXIT':
+        continue_conversion = False
+
+    else:
+        print(convert_text(text_input))
+
+print('Goodbye!')
+print(convert_text('GOODBYE!'))
